df1309d191fa155aea3839ff9e3edd9aa9e36910
Michał J. Gąsior <michaljgasior@gmail.com>
2019-09-27 22:47:53 +0200

    Cleanup

---
+++ Numpuk2/Numpuk2/Hubs/FilesHub.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Numpuk2.Hubs
+{
+    public class FilesHub : Hub
+    {
+        public async Task SendFiles(File[] filePaths)
+        {
+            Console.Write(filePaths);
+            await Clients.All.SendAsync("FilesAccepted", "Pliki w trakcie procesowania...");
+            Thread.Sleep(5000);
+
+            int TOTAL_COUNT = filePaths.Length;
+            int count = 0;
+            foreach (File file in filePaths)
+            {
+                var result = new Progress
+                {
+                    FileName = Path.GetFileName(file.Path),
+                    FileNumber = ++count,
+                    TotalCount = TOTAL_COUNT
+                };
+                await Clients.All.SendAsync("FileProcessed", result);
+                Thread.Sleep(5000);
+            }
+            await Clients.All.SendAsync("AllFilesDone");
+        }
+    }
+
+    public class File
+    {
+        public string Path { get; set; }
+    }
+
+    public class Progress
+    {
+        public int FileNumber { get; set; }
+        public int TotalCount { get; set; }
+        public string FileName { get; set; }
+    }
+}
--- Numpuk2/Numpuk2/Startup.cs
+++ Numpuk2/Numpuk2/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Numpuk2.Hubs;
 
 namespace Numpuk2
@@ -23,7 +16,6 @@ namespace Numpuk2
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
@@ -39,7 +31,6 @@ namespace Numpuk2
             services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -59,6 +50,7 @@ namespace Numpuk2
             {
                 endpoints.MapControllers();
                 endpoints.MapHub<ChatHub>("/chatHub");
+                endpoints.MapHub<FilesHub>("/filesHub");
             });
         }
     }
