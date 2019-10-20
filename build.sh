babd5ff4ffb42f62262bdab989b9282eec174f45
Michał J. Gąsior <michaljgasior@gmail.com>
2019-10-20 13:02:05 +0200

    Build script

---
+++ build.sh
@@ -0,0 +1,21 @@
+echo "Preparing UI build"
+cd ./numpuk-ui
+npm run build
+cd ..
+
+echo "Copying built files to server project"
+cp -a ./numpuk-ui/build/. ./Numpuk2/Numpuk2/wwwroot/
+
+echo "Deleting UI build directory"
+rm -rf ./numpuk-ui/build/
+
+echo "Make server build"
+cd ./Numpuk2
+dotnet build
+
+echo "Deleting wwwroot"
+rm -rf ./Numpuk2/wwwroot/
+
+echo "Reseting repository"
+cd ./../../
+git reset --hard
